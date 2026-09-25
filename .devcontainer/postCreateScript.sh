f29e156a7393b8b7d31c5378b133dc839c8f3c4c
Varnika <noreply@github.com>
2026-09-25 05:00:06 +0000

    Fix: Sonar Issues

--- .devcontainer/postCreateScript.sh
+++ .devcontainer/postCreateScript.sh
@@ -7,14 +7,20 @@ readonly GITHUB_URL="https://raw.githubusercontent.com"
 readonly ALIAS_SRC_URL="${GITHUB_URL}/gvatsal60/Linux-Aliases/HEAD/install.sh"
 
 ##########################################################################################
+# Functions
+##########################################################################################
+curl_https() {
+    if ! command -v curl >/dev/null 2>&1; then
+        echo "Error: curl is not installed. Aborting." >&2
+        return 1
+    fi
+    curl -fsSL --proto '=https' "$@"
+}
+
+##########################################################################################
 # Main Script
 ##########################################################################################
 
 # Install Linux aliases from external script using curl and execute immediately
 # Note: Make sure to review scripts fetched from external sources for security reasons
-if command -v curl >/dev/null 2>&1; then
-    curl -fsSL "${ALIAS_SRC_URL}" | sh
-else
-    echo "Error: curl is not installed. Unable to use Linux aliases"
-    exit 1
-fi
+curl_https "${ALIAS_SRC_URL}" | sh
